3e62fd41db20cb2008477df12d45b5ebe97d34ec
Sfeeney3 <sfeeney3@gmail.com>
2023-11-23 09:39:02 -0700

    Update dbService.py

--- services/dbService.py
+++ services/dbService.py
@@ -9,19 +9,14 @@ class dbObject:
     def __init__(self):
         print("Initializing dbObject")
         
-        #self.table = 'aud_vid_trans'
-        #self.user = 'MLFINETLID'
-        #self.password = 'e6QMhGxB49sjR6Kx'
-        #self.host = 's1.vedastrading.com'
-        #self.port = 3307
-        #self.database = 'MLFIN'
+
         
-        self.table = 'aud_vid_trans'
-        self.user = 'MLFINETLID'
-        self.password = 'e6QMhGxB49sjR6Kx'
-        self.host = '192.168.0.56'
-        self.port = 3306
-        self.database = 'MLFIN'
+        self.table = 
+        self.user = 
+        self.password = 
+        self.host = 
+        self.port = 
+        self.database = '
             
             
         self.con = self.get_connection()
